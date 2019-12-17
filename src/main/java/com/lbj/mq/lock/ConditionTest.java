f963dc347e1108fcaeca884f4b398f23d11c0552
PeterLu798 <1649675756@qq,com>
2019-12-17 16:33:12 +0800

    update README

--- src/main/java/com/lbj/mq/lock/ConditionTest.java
+++ src/main/java/com/lbj/mq/lock/ConditionTest.java
@@ -1,14 +1,14 @@
 package com.lbj.mq.lock;
 
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.*;
 
 public class ConditionTest {
     private static Lock lock = new ReentrantLock();
     private static Condition condition = lock.newCondition();
 
     public static void main(String[] args) throws InterruptedException {
+        ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+        readWriteLock.readLock();
         condition.await();
     }
 }
